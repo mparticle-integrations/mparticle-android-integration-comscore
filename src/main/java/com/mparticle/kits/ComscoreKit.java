2343d9c34cdfffb1e75b05e03271b4d9880fbde6
Will Passidomo <wpassidomo@mparticle.com>
2018-07-30 14:36:32 -0400

    Update

--- src/main/java/com/mparticle/kits/ComscoreKit.java
+++ src/main/java/com/mparticle/kits/ComscoreKit.java
@@ -10,8 +10,8 @@ import com.comscore.PublisherConfiguration;
 import com.comscore.util.log.LogLevel;
 import com.mparticle.MPEvent;
 import com.mparticle.MParticle;
-import com.mparticle.kits_core.KitIntegration;
-import com.mparticle.kits_core.ReportingMessage;
+import com.mparticle.kits.core.KitIntegration;
+import com.mparticle.kits.core.ReportingMessage;
 
 import java.util.HashMap;
 import java.util.LinkedList;
